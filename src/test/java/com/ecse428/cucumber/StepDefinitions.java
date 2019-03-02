9731f57cd8f1042aa969fa0d4469ad8828d82779
EBruchet <evan.bruchet@mail.mcgill.ca>
2019-03-02 16:37:29 -0500

    Putting the trash out there

--- src/test/java/com/ecse428/cucumber/StepDefinitions.java
+++ src/test/java/com/ecse428/cucumber/StepDefinitions.java
@@ -18,7 +18,7 @@ import java.net.MalformedURLException;
 public class StepDefinitions {
 
     private WebDriver driver;
-    private final String PATH_TO_CHROME_DRIVER = "...";
+    private final String PATH_TO_CHROME_DRIVER = "D:/UbuntuShared/chromedriver";
     private final String EMAIL_URL = "https://www.amazon.ca/Monoprice-115365-Select-Mini-Printer/dp/B01FL49VZE/ref=sr_1_1?ie=UTF8&qid=1488132110&sr=8-1&keywords=3d+printer";
     private final String EMAIL_SUBJECT = "Monoprice 115365 Monoprice Select Mini 3D Printer";
 //    private final String DELETE_BTN_NAME = "submit.delete.C3NLW69582M4B4";
